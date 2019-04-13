0d3333e78017dfbde21b1b40ea6b1d756ac9f932
yujin <wkddvwls1@gmail.com>
2019-04-13 17:52:37 +0900

    햄버거 시도

--- app/src/main/java/com/cau/goodbody/MainActivity.java
+++ app/src/main/java/com/cau/goodbody/MainActivity.java
@@ -1,13 +1,19 @@
 package com.cau.goodbody;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar mainToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainToolbar=findViewById(R.id.main_toolbar);
+        setSupportActionBar(mainToolbar);
     }
 }
